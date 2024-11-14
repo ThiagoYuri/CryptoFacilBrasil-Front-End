6d74d3338ce1f301366e383a51ce0068ef5511ef
Thiago Yuri Oliveira Monteiro <thiago.monteiro@whitecube.com.br>
2024-11-14 17:16:52 -0300

    Add routing's

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
 import NavBar from "./components/NavBar/NavBar";
 import Home from "./pages/Home/Home";
-
+import About from "./pages/About/About";
+import Terms from "./pages/Terms/Terms";
+//import Contact from "./pagesContato";
 function App() {
   return (
+    <Router>
     <div className="App">
       <NavBar />
-      <Home/>
+      <div className="container mx-auto mt-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/terms" element={<Terms />} />
+          {/*<Route path="/contact" element={<Contact />} />*/}
+        </Routes>
+      </div>
     </div>
+    </Router>
   );
 }
 
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,48 +1,35 @@
-
 import React from "react";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
-  //const [isOpen, setIsOpen] = useState(false);
-
   return (
     <nav className="bg-gray-900 text-white p-2 shadow-lg w-full bg-opacity-10">
       <div className="container mx-auto flex justify-center">
-
-        {/* Links da Navbar */}
         <ul className="md:flex space-y-2 md:space-y-0 md:space-x-6 text-lg">
           <li>
-            <a href="1" className="text-center block py-2 px-4 hover:bg-gray-800 rounded transition-colors duration-200">
-              Início
-            </a>
+            <Link to="/" className="text-center block py-2 px-4 hover:bg-gray-800 rounded transition-colors duration-200">
+              Home
+            </Link>
           </li>
           <li>
-            <a href="2" className="text-center block py-2 px-4 hover:bg-gray-800 rounded transition-colors duration-200">
+            <Link to="/about" className="text-center block py-2 px-4 hover:bg-gray-800 rounded transition-colors duration-200">
               Sobre
-            </a>
+            </Link>
           </li>
           <li>
-            <a href="3" className="text-center block py-2 px-4 hover:bg-gray-800 rounded transition-colors duration-200">
+            <Link to="/terms" className="text-center block py-2 px-4 hover:bg-gray-800 rounded transition-colors duration-200">
               Termos
-            </a>
+            </Link>
           </li>
           <li>
-            <a href="3" className="text-center block py-2 px-4 hover:bg-gray-800 rounded transition-colors duration-200">
+            <Link to="/contact" className="text-center block py-2 px-4 hover:bg-gray-800 rounded transition-colors duration-200">
               Contato
-            </a>
+            </Link>
           </li>
         </ul>
-
       </div>
     </nav>
-
-
   );
 };
-
-NavBar.propTypes = {};
-
-NavBar.defaultProps = {};
-
-
 
 export default NavBar;

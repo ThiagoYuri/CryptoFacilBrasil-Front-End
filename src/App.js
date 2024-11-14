a0881b888c320b09cca036cad2b501e6b01d70de
Thiago Yuri Oliveira Monteiro <thiago.monteiro@whitecube.com.br>
2024-11-14 17:38:40 -0300

    Adding: AnimatedRoutes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import { AnimatePresence, motion } from "framer-motion";
 import './App.css';
 import NavBar from "./components/NavBar/NavBar";
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
 import Terms from "./pages/Terms/Terms";
-//import Contact from "./pagesContato";
+// import Contact from "./pages/Contact/Contact";
+
+function AnimatedRoutes() {
+  const location = useLocation();
+
+  return (
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route
+          path="/"
+          element={
+            <motion.div
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: -20 }}
+              transition={{ duration: 0.3 }}
+            >
+              <Home />
+            </motion.div>
+          }
+        />
+        <Route
+          path="/about"
+          element={
+            <motion.div
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: -20 }}
+              transition={{ duration: 0.3 }}
+            >
+              <About />
+            </motion.div>
+          }
+        />
+        <Route
+          path="/terms"
+          element={
+            <motion.div
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: -20 }}
+              transition={{ duration: 0.3 }}
+            >
+              <Terms />
+            </motion.div>
+          }
+        />
+        {/* Uncomment and add motion div for Contact route if needed */}
+        {/* <Route path="/contact" element={<motion.div><Contact /></motion.div>} /> */}
+      </Routes>
+    </AnimatePresence>
+  );
+}
+
 function App() {
   return (
     <Router>
-    <div className="App">
-      <NavBar />
-      <div className="container mx-auto mt-4">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/terms" element={<Terms />} />
-          {/*<Route path="/contact" element={<Contact />} />*/}
-        </Routes>
+      <div className="App">
+        <NavBar />
+        <div className="container mx-auto mt-4">
+          <AnimatedRoutes />
+        </div>
       </div>
-    </div>
     </Router>
   );
 }
